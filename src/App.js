d51925a745ae1b3d532170cabfbaa65e7a866f18
Cody Tomesch <tomeschcody@gmail.com>
2018-11-10 14:11:02 -0700

    moved routes to AApp.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './Components/login/login'
-import router from './router'
+import { Switch, Route } from 'react-router-dom';
+import Login from './Components/Login/login'
+import Dashboard from './Components/dashboardLanding/dashboardLanding'
+import OrderForm from './Components/orderForm/orderForm'
+import AddInventory from './Components/addInventoryForm/addInventoryForm'
 import AuthCheck from './Components/AuthCheck'
 
 
@@ -12,7 +15,12 @@ class App extends Component {
     return (
       <div className="App">
       <AuthCheck>
-        {router}
+        <Switch>
+          <Route component={Login} exact path='/'/>
+          <Route component={Dashboard} exact path='/home'/>
+          <Route component={OrderForm} exact path='/order'/>
+          <Route component={AddInventory} exact path='/add'/>
+        </Switch>
       </AuthCheck>
       </div>
     )
