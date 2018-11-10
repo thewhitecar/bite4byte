80707d8360646caf6a3be7034feef72b3caf46d5
zach <zaspringer@gmail.com>
2018-11-10 11:58:40 -0700

    Made the endpoint to get inventory and family by id. also brought in some sql files and made routes in index.js

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,10 @@
 CREATE TABLE pantry (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     pantry_name TEXT
 );
 
 CREATE TABLE coordinator (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name TEXT, 
     username TEXT,
     password TEXT,
@@ -12,19 +12,19 @@ CREATE TABLE coordinator (
 );
 
 CREATE TABLE item (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     item_name TEXT
 );
 
 CREATE TABLE item_pantry_link (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     item_id INTEGER REFERENCES item,
     pantry_id INTEGER REFERENCES pantry,
     quantity INTEGER
 );
 
 CREATE TABLE family (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     family_name TEXT,
     picked_up BOOLEAN,
     last_pickup_date DATE,
@@ -32,6 +32,6 @@ CREATE TABLE family (
 );
 
 CREATE TABLE historical_pantry_data (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     pantry_id INTEGER REFERENCES pantry
 );
--- server/controllers/inventoryController.js
+++ server/controllers/inventoryController.js
@@ -6,6 +6,21 @@ module.exports = {
 
     },
     getInventory(req, res) {
+        let db = req.app.get('db')
+        let {pantryId} = req.params 
+        db.inventory.get_inventory({id: pantryId}).then(dbRes => {
+            db.family.get_family_by_pantry_id({id: pantryId}).then(dbRes2 => {
+                let formatted = {
+                    inventory: dbRes,
+                    families: dbRes2
+                }
+                res.status(200).send(formatted)
+            }).catch(err => {
+                res.status(500).send(err)
+            })
+        }).catch(err => {
+            res.status(500).send(err)
+        })
 
     },
     updateInventoryQuantity(req, res) {
