31dd3cd435a20bf977749da0ef4654ec031de9c9
Cody Tomesch <tomeschcody@gmail.com>
2018-11-10 14:14:19 -0700

    undid routing changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import Login from './Components/Login/login'
-import Dashboard from './Components/dashboardLanding/dashboardLanding'
-import OrderForm from './Components/orderForm/orderForm'
-import AddInventory from './Components/addInventoryForm/addInventoryForm'
+import router from './router'
 import AuthCheck from './Components/AuthCheck'
 
 
@@ -15,12 +11,7 @@ class App extends Component {
     return (
       <div className="App">
       <AuthCheck>
-        <Switch>
-          <Route component={Login} exact path='/'/>
-          <Route component={Dashboard} exact path='/home'/>
-          <Route component={OrderForm} exact path='/order'/>
-          <Route component={AddInventory} exact path='/add'/>
-        </Switch>
+        {router}
       </AuthCheck>
       </div>
     )
